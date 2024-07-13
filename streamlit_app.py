c4ba4be4089e74275a627617cb211f55518e7753
bhuvanikannan55 <bhuvanikannan@gmail.com>
2024-07-13 00:39:36 -0400

    Update streamlit_app.py
    comm

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ session = cnx.session()
 
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('search_on'))
+st.dataframe(data=my_dataframe,use_container_width =True)
+st.stop()
 ingredients_list = st.multiselect(
     "choose upto 5 ingredients",my_dataframe)
 
